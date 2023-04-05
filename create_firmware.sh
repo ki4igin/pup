b987651e8c59fc22c721d805e421360d81cc4a8e
artem <ki4igin1992@outlook.com>
2023-04-05 20:11:28 +0300

    fix create_frimware

--- create_firmware.sh
+++ create_firmware.sh
@@ -1,14 +1,14 @@
 FIRMWARE_DIR=firmware
 
-INFO=$(make info)
+INFO=$(make info RELEASE=1)
 VERSION=$(echo $INFO | sed -e 's/.*Version:[[:space:]]*[^[:space:]]*[[:space:]]*//; s/-.*$//')
 TARGET=$(echo $INFO | sed -e 's/.*Target:[[:space:]]*[^[:space:]]*[[:space:]]*//; s/[[:space:]].*$//')
 BUILD_DIR=$(echo $INFO | sed -e 's/.*Build to:[[:space:]]*[^[:space:]]*[[:space:]]*//; s/[[:space:]].*$//')
 
 make check_version
-make build
-
+make build RELEASE=1
+echo $BUILD_DIR
 cp $BUILD_DIR/$TARGET.hex $FIRMWARE_DIR/${TARGET}_v$VERSION.hex
 
-git add $FIRMWARE_DIR/${TARGET}_v$VERSION.hex
-git commit --amend --no-edit
+# git add $FIRMWARE_DIR/${TARGET}_v$VERSION.hex
+# git commit --amend --no-edit
